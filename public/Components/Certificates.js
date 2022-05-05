6d51e6cd21a011017c2dc5ae6d159f62c6ae11a4
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-05-05 17:36:46 -0400

    updates

---
+++ public/Components/Certificates.js
@@ -0,0 +1,69 @@
+// import React, { Component } from "react";
+// import "~slick-carousel/slick/slick.css";
+// import "~slick-carousel/slick/slick-theme.css";
+// import Slider from "react-slick";
+
+import React, { Component } from "react";
+import Image from "next/image";
+// import ReactDOM from "react-dom";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
+
+import Image1 from "../public/01.webp";
+import Image2 from "../public/02.webp";
+import Image3 from "../public/03.webp";
+import Image4 from "../public/04.webp";
+import Image5 from "../public/05.webp";
+import Image6 from "../public/06.webp";
+import Image7 from "../public/07.webp";
+import Image8 from "../public/08.webp";
+import Image9 from "../public/09.webp";
+import Image10 from "../public/10.webp";
+import Image11 from "../public/11.webp";
+import Image12 from "../public/12.webp";
+
+class SimpleSlider extends Component {
+  render() {
+    return (
+      <Carousel className="max-w-[1000px] pb-20 mx-auto" showThumbs={false}>
+        <div>
+          <Image src={Image1} alt="Image1" />
+        </div>
+        <div>
+          <Image src={Image2} alt="Image2" />
+        </div>
+        <div>
+          <Image src={Image3} alt="Image3" />
+        </div>
+        <div>
+          <Image src={Image4} alt="Image4" />
+        </div>
+        <div>
+          <Image src={Image5} alt="Image5" />
+        </div>
+        <div>
+          <Image src={Image6} alt="Image6" />
+        </div>
+        <div>
+          <Image src={Image7} alt="Image7" />
+        </div>
+        <div>
+          <Image src={Image8} alt="Image8" />
+        </div>
+        <div>
+          <Image src={Image9} alt="Image9" />
+        </div>
+        <div>
+          <Image src={Image10} alt="Image10" />
+        </div>
+        <div>
+          <Image src={Image11} alt="Image11" />
+        </div>
+        <div>
+          <Image src={Image12} alt="Image12" />
+        </div>
+      </Carousel>
+    );
+  }
+}
+export default SimpleSlider;
---
+++ public/Components/Skills.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+import HTML from "../public/assets/html.webp";
+import CSS from "../public/assets/css.webp";
+import JS from "../public/assets/javascript.webp";
+import ReactImg from "../public/assets/react.webp";
+import Node from "../public/assets/node.webp";
+import FireBase from "../public/assets/firebase.webp";
+import GitHub from "../public/assets/github.webp";
+import Tailwind from "../public/assets/tailwind.webp";
+import Mongo from "../public/assets/mongo.webp";
+import Image from "next/image";
+
+const Skills = () => {
+  const skills = [
+    { title: "HTML", image: HTML },
+    { title: "CSS", image: CSS },
+    { title: "JS", image: JS },
+    { title: "ReactImg", image: ReactImg },
+    { title: "Node", image: Node },
+    { title: "Firebase", image: FireBase },
+    { title: "Github", image: GitHub },
+    { title: "TailwindCSS", image: Tailwind },
+    { title: "Mongo", image: Mongo },
+  ];
+  return (
+    <div>
+      <div className="sm:w-auto grid grid-cols-3 sm:grid-cols-3 gap-2 text-center py-8 p-4">
+        {skills.map((skill) => (
+          <div
+            className="shadow-md shadow-slate-500 hover:scale-110 border-2 border-transparent hover:border-slate-500 duration-700 pt-6"
+            key={skill.title}
+          >
+            <div className="w-20 mx-auto shadow-blue-600/50 drop-shadow-lg">
+              <Image
+                src={skill.image}
+                alt="CSS Icon"
+                width={800}
+                height={800}
+              />
+            </div>
+            <p className="my-4">{skill.title}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
