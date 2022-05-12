a0e99f08f8da1924ae0fec8853f47d2a0c621470
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-05-12 15:56:44 -0400

    update

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,8 @@ const index = () => {
           />
           <meta
             property="og:image"
-            content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+            content={MetaImage}
+      
           />
           <meta property="twitter:card" content="summary_large_image" />
           <meta property="twitter:url" content="https://www.dmitriymalayev.com/" />
@@ -43,7 +44,7 @@ const index = () => {
           />
           <meta
             property="twitter:image"
-            content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
+            content={MetaImage}
           />
 
           <link href="https://assets.calendly.com/assets/external/widget.css" rel="stylesheet" />
