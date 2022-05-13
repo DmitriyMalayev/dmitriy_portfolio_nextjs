bbe39ac98b5476fcc65fa6c7cc34d31c7069eadf
Dmitriy Malayev <dmitriy.malayev@gmail.com>
2022-05-13 15:01:52 -0400

    update

--- Components/SectionWrapper.js
+++ Components/SectionWrapper.js
@@ -3,35 +3,25 @@
 import Image from "next/image";
 import Photo from "../public/photo.webp";
 
-const SectionWrapper = ({
-  id,
-  title,
-  subtitle,
-  text,
-  text2,
-  text3,
-  about,
-  children,
-}) => {
+const SectionWrapper = ({ id, title, subtitle, text, text2, text3, about, children }) => {
   return (
-    <section
-      id={id}
-      className="w-full lg:h-screen text-slate-300 py-12 mb-32"
-    >
+    <section id={id} className="w-full lg:h-screen text-slate-300 py-12 mb-32">
       <div className="flex flex-col justify-center items-center w-full h-full mx-auto">
         <div
           className={`max-w-[1000px] w-full grid ${
             text && "sm:grid-cols-2"
-          } gap-2 place-content-start sm:place-content-center `}
+          } gap-2 place-content-start sm:place-content-center`}
         >
           <div
             className={`pl-4 mb-4 ml-4 items-start border-l-4 flex flex-col ${
-              text
-                ? "sm:items-end sm:text-right"
-                : "sm:items-center"
+              text ? "sm:items-end sm:text-right" : "sm:items-center"
             } border-orange-300 space-y-4 sm:pl-0 sm:ml-0 sm:border-l-0 sm:space-y-0 sm:translate-y-[-60px]`}
           >
-            <div className={`text-3xl md:text-5xl font-bold inline border-b-4 border-orange-600 text-cyan-100 sm:px-2 pb-2 sm:mb-2 ${ text && 'sm:translate-x-1/2 lg:translate-x-0'}`}>
+            <div
+              className={`text-3xl md:text-5xl font-bold inline border-b-4 border-orange-600 text-cyan-100 sm:px-2 pb-2 sm:mb-2 ${
+                text && "sm:translate-x-1/2 lg:translate-x-0"
+              }`}
+            >
               {title}
             </div>
             <div
@@ -39,9 +29,7 @@ const SectionWrapper = ({
                 text ? "justify-end" : "text-center"
               } flex flex-col font-bold font-dance text-lg md:text-2xl text-orange-300`}
             >
-              <p className={`${text && 'sm:max-w-[250px]'} sm:self-end sm:p-2 `}>
-                {subtitle}
-              </p>
+              <p className={`${text && "sm:max-w-[250px]"} sm:self-end sm:p-2 `}>{subtitle}</p>
               {about && (
                 <div className="sm:w-[50%] md:w-[30%] lg:w-[40%]  sm:self-end px-2  mt-4 mr-4 sm:mr-0">
                   <Image src={Photo} alt="" />
@@ -51,8 +39,8 @@ const SectionWrapper = ({
           </div>
 
           {text && (
-            <div className="sm:text-md lg:text-2xl leading-2 text-slate-50 p-3">
-              <p className="pb-2 pt-3">{text}</p>
+            <div className="sm:text-md lg:text-2xl leading-2 text-slate-50 px-2 md:pt-2">
+              <p className="pb-2 pt-2">{text}</p>
               <p className="pb-2 pt-2">{text2}</p>
               <p className="pb-2 pt-2">{text3}</p>
               <p></p>
